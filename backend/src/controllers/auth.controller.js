0792692cb4bc07c0b355268f7eb74953ba4a3a7d
Abhilash Kashyap <abhilash@Ubuntu64.myguest.virtualbox.org>
2025-06-27 11:41:00 +0530

    authentication routes added, middleware added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+//import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req,res)=>{
     const {fullName,email,password}=req.body;
 
@@ -92,3 +94,31 @@ export const logout = async (req,res)=>{
     }
 }
 
+export const updateProfile = async (req,res)=>{
+try{
+    const {profilePic}=req.body;
+    const userId = req.user._id //possible with the help of the protectroute function
+    if(!profilePic){
+        return res.status(400).json({message: "Profile Pic is reqd"})
+    }
+    const uploadRespone = await cloudinary.uploader.upload(profilePic)
+    const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadRespone.secure_url}, {new:true})
+
+    res.status(200).json({message:"Updated user"})
+
+}
+catch (error){
+    console.log("Error in update profile",error.message)
+    res.status(500).json({message: "Internal Server error"});
+}
+}
+
+export const checkAuth = (req,res)=>{
+    try{
+        res.status(200).json(req.user);
+    }
+    catch(error){
+        console.log("Error in checkAuth controller",error.message)
+        res.status(500).json({message: "internal server error"})
+    }
+}
