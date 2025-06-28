a540d5aba584e64a8002cd27703d21593f883ae0
Abhilash Kashyap <abhilash@Ubuntu64.myguest.virtualbox.org>
2025-06-28 11:16:44 +0530

    modified message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -18,10 +18,50 @@ export const getUsersForSidebar = async (req,res)=>{
 export const getMessages = async (req,res)=>{
     try{
         const { id:userToChatId} = req.params
-        const senderId= req.user._id
-        const messages= await Message
-    }
-    catch{
+        const myId= req.user._id
+        const messages= await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiver:myId}
+            ]
+        })
 
+        res.sttaus(200).json(messages)
+    }
+    catch(error){
+        console.error("Error in getMessages: ", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+
+    }
+}
+
+
+export const sendMessage = async (req,res)=>{
+    try{
+        const {text,image} = req.body;
+        const {id: receiverId}= req.params
+        const senderId = req.user._id //currently logged in user
+
+        let imageUrl;
+        if(image){
+            //Upload base64 image to cloudinary
+            const uploadRespone = await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url
+        }
+        //creating the message
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        await newMessage.save()
+
+        res.status(201).json(newMessage)
+        }
+    catch(error){
+        console.error("Error in sendMessage: ", error.message)
+        res.status(500).json({error: "Internal Server Error"})
     }
 }
