ab60f64d00d00ec9501a3e346b89b7c4db08a940
Abhilash Kashyap <abhilash@Ubuntu64.myguest.virtualbox.org>
2025-06-29 20:52:10 +0530

    cloudinary file fixed, profile page and settings page added

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,9 @@ dotenv.config()
 const app=express();
 const PORT=process.env.PORT
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
